1431874fff01046f6166c673b0e9b417f8d839bd
Carlos Hernandez <c90.hdz@gmail.com>
2025-11-16 21:32:03 -0500

    Add PWA manifest and service worker

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "iron-pulse-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Install - cache core assets
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(() => {
+        // If some assets fail (like missing icons), just skip them
+      });
+    })
+  );
+});
+
+// Activate - cleanup old caches if you change CACHE_NAME later
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch - try network first, fallback to cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
